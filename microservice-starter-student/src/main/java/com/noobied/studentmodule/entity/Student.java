55cd0cc73a718c3b0604a4f7910ae43924b37dd8
suriya-prakash-s <suriyaprakashs.official@gmail.com>
2023-04-18 17:36:27 +0530

    Updated application.properties file and created student entity

--- microservice-starter-student/src/main/java/com/noobied/studentmodule/entity/Student.java
+++ microservice-starter-student/src/main/java/com/noobied/studentmodule/entity/Student.java
@@ -1,9 +1,26 @@
 package com.noobied.studentmodule.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//Student entity class with id, fname, lname as attributes
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
 public class Student {
-
+    //student_id serves as a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id", nullable = false)
     private long id;
+    //column first_name to store student first name
+    @Column(name = "first_name", nullable = false)
     private String fname;
+    //column last_name to store student last name
+    @Column(name = "last_name", nullable = false)
     private String lname;
 
 }
